3341abb5a358dfdc030bd24f2ff6cbf615a6f1a7
Taffyyy123 <tsolmn9@gmail.com>
2025-02-05 18:01:30 +0800

    d

--- models/postModel.js
+++ models/postModel.js
@@ -1,18 +1,17 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const PostSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  subtitle: { type: String, required: true },
-  content: { type: String, required: true },
-  authorId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
+const PostSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    infoTitle: { type: String },
+    infoContent: { type: String },
+    createdAt: { type: Date, default: Date.now },
+    imageUrl: { type: String },
   },
-  categoryId: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
-  imageUrl: { type: String },
-});
+  { timestamps: true }
+);
 
 const postModel = mongoose.model("Post", PostSchema);
 module.exports = postModel;
